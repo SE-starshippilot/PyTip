19ca1b91c45c1fb0392179b8c7d0640f8021d800
Tianhao <120090472@link.cuhk.edu.cn>
2023-11-25 14:31:04 +0800

    added first UI page and modified retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -1,21 +1,24 @@
+from flask import Flask, request, jsonify
 import gensim.downloader
 from gensim.models import KeyedVectors
 import numpy as np
 
-#Load pre-trained word vectors
+app = Flask(__name__)
+
+# Load pre-trained word vectors
 glove_vectors = gensim.downloader.load('glove-wiki-gigaword-100')
 
-# Sample query and results
-# query = "How to draw a line"
+# Sample results
 result_dict = {
     'plot a pie chart': 'matplotlib.pyplot.pie',
     'plot a bar chart': 'matplotlib.pyplot.bar',
     'plot a line chart': 'matplotlib.pyplot.plot',
     'clear the current axes': 'matplotlib.pyplot.cla'
 }
+
 # Tokenize and obtain word embeddings for each sentence
 def get_sentence_embedding(sentence):
-    words = sentence.lower().split()  # Tokenization might need further adjustments
+    words = sentence.lower().split()
     vectors = [glove_vectors[word] for word in words if word in glove_vectors]
     return np.mean(vectors, axis=0) if vectors else np.zeros(glove_vectors.vector_size)
 
@@ -31,19 +34,24 @@ def convert_to_declarative(question):
 
 result_embeddings = [get_sentence_embedding(result) for result in result_dict.keys()]
 
-while True:
-    # Calculate cosine similarity between query and each result
-    query = input('Please enter a question: ')
-    if not query: break
-    query_declared = convert_to_declarative(query)
-    query_embedding = get_sentence_embedding(query_declared)
-    similarity_scores = [np.dot(query_embedding, result_emb) / (np.linalg.norm(query_embedding) * np.linalg.norm(result_emb)) for result_emb in result_embeddings]
+@app.route('/query', methods=['POST'])
+def process_query():
+    data = request.get_json()
+    if data and 'query' in data:
+        query = data['query']
+        query_declared = convert_to_declarative(query)
+        query_embedding = get_sentence_embedding(query_declared)
+        similarity_scores = [np.dot(query_embedding, result_emb) / (np.linalg.norm(query_embedding) * np.linalg.norm(result_emb)) for result_emb in result_embeddings]
 
-    # Rank results based on similarity scores
-    ranked_results = sorted(zip(result_dict.keys(), similarity_scores), key=lambda x: x[1], reverse=True)
+        # Rank results based on similarity scores
+        ranked_results = sorted(zip(result_dict.keys(), similarity_scores), key=lambda x: x[1], reverse=True)
 
-    # Print ranked results
-    print(f'Query: {query}\nReturn results:\n')
-    for idx, (result, score) in enumerate(ranked_results):
-        print(f"#{idx}: {result_dict[result]} ({result}, {score})")
-    print('*' * 50 + '\n')
+        # Prepare response
+        ranked_results = [{'function': result_dict[result], 'score': score} for result, score in ranked_results]
+
+        return jsonify({'results': ranked_results})
+
+    return jsonify({'error': 'Invalid input'})
+
+if __name__ == '__main__':
+    app.run(port=8080)  # Run Flask app on port 8080
