0b4195eba28145304e88d6d8d10cb2c59b6bf521
Tianhao <120090472@link.cuhk.edu.cn>
2023-11-25 19:31:30 +0800

    made the server dummy so it can start right away

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ import socket
 import numpy as np
 import gensim.downloader
 
-def get_sentence_embedding(sentence):
-    words = sentence.lower().split()  # Tokenization might need further adjustments
-    vectors = [glove_vectors[word] for word in words if word in glove_vectors]
-    return np.mean(vectors, axis=0) if vectors else np.zeros(glove_vectors.vector_size)
+# def get_sentence_embedding(sentence):
+#     words = sentence.lower().split()  # Tokenization might need further adjustments
+#     vectors = [glove_vectors[word] for word in words if word in glove_vectors]
+#     return np.mean(vectors, axis=0) if vectors else np.zeros(glove_vectors.vector_size)
 
 def convert_to_declarative(question):
     question_words = ['who', 'what', 'when', 'where', 'why', 'how', 'is', 'are', 'am', 'was', 'were', 'do', 'does', 'did', 'can', 'could', 'will', 'would', 'shall', 'should']
@@ -34,20 +34,20 @@ def start_server(host, port):
                 print(f'Exit')
                 break
             print(f"Received message: {query}")
-            query_declared = convert_to_declarative(query)
-            query_embedding = get_sentence_embedding(query_declared)
-            similarity_scores = [np.dot(query_embedding, result_emb) / (np.linalg.norm(query_embedding) * np.linalg.norm(result_emb)) for result_emb in result_embeddings]
+            # query_declared = convert_to_declarative(query)
+            # query_embedding = get_sentence_embedding(query_declared)
+            # similarity_scores = [np.dot(query_embedding, result_emb) / (np.linalg.norm(query_embedding) * np.linalg.norm(result_emb)) for result_emb in result_embeddings]
 
-            # Rank results based on similarity scores
-            ranked_results = sorted(zip(result_dict.keys(), similarity_scores), key=lambda x: x[1], reverse=True)
+            # # Rank results based on similarity scores
+            # ranked_results = sorted(zip(result_dict.keys(), similarity_scores), key=lambda x: x[1], reverse=True)
 
-            # Print ranked results
-            message = [f'Query: {query}\nReturn results:']
-            for idx, (result, score) in enumerate(ranked_results):
-                message.append(f"#{idx}: {result_dict[result]} ({result}, {score})")
-            message.append('*' * 50 + '\n')
-            message = '\n'.join(message)
-            client_socket.sendall(message.encode("utf-8"))
+            # # Print ranked results
+            # message = [f'Query: {query}\nReturn results:']
+            # for idx, (result, score) in enumerate(ranked_results):
+            #     message.append(f"#{idx}: {result_dict[result]} ({result}, {score})")
+            # message.append('*' * 50 + '\n')
+            # message = '\n'.join(message)
+            # client_socket.sendall(message.encode("utf-8"))
 
 
         client_socket.close()
@@ -57,16 +57,16 @@ if __name__ == "__main__":
     HOST = '127.0.0.1'  # Localhost
     PORT = 8080  # Replace with your desired port number
     #Load pre-trained word vectors
-    glove_vectors = gensim.downloader.load('glove-wiki-gigaword-100')
+    # glove_vectors = gensim.downloader.load('glove-wiki-gigaword-100')
 
-    result_dict = {
-        'plot a pie chart': 'matplotlib.pyplot.pie',
-        'plot a bar chart': 'matplotlib.pyplot.bar',
-        'plot a line chart': 'matplotlib.pyplot.plot',
-        # 'add an axes to the current figure or retrieve an existing axes': 'matplotlib.pyplot.axes',
-        # 'add a subplot to the current figure.': 'matplotlib.pyplot.subplot',
-        'clear the current axes': 'matplotlib.pyplot.cla',
-    }
-    # Tokenize and obtain word embeddings for each sentence
-    result_embeddings = [get_sentence_embedding(result) for result in result_dict.keys()]
+    # result_dict = {
+    #     'plot a pie chart': 'matplotlib.pyplot.pie',
+    #     'plot a bar chart': 'matplotlib.pyplot.bar',
+    #     'plot a line chart': 'matplotlib.pyplot.plot',
+    #     # 'add an axes to the current figure or retrieve an existing axes': 'matplotlib.pyplot.axes',
+    #     # 'add a subplot to the current figure.': 'matplotlib.pyplot.subplot',
+    #     'clear the current axes': 'matplotlib.pyplot.cla',
+    # }
+    # # Tokenize and obtain word embeddings for each sentence
+    # result_embeddings = [get_sentence_embedding(result) for result in result_dict.keys()]
     start_server(HOST, PORT)
