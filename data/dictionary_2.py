24a8d0360d7eadd9f0ac3271cb361d5c2b928f65
BaoRui666 <86188582+BaoRui666@users.noreply.github.com>
2023-11-27 19:47:56 +0800

    调整了一下格式

--- data/dictionary_2.py
+++ data/dictionary_2.py
@@ -4,20 +4,25 @@ import json
 with open("numpy.txt", "r") as file:
     lines = file.readlines()
 
-# 创建一个空的字典来存储数据
-data_ALL = []
+# 创建一个空的列表来存储JSON结构
+json_list = []
 
 # 遍历行号和内容
-for i, line in enumerate(lines):
-    data = {}
-    # 奇数行存储为"function name"
-    if i % 2 == 0:
-        data["function name"] = line.strip()
-    # 偶数行存储为"function description"
-    else:
-        data["function description"] = line.strip()
-    data_ALL.append(data)
-# 将数据写入JSON文件
-with open("numpy.json", "w") as file:
-    file.write(str(data_ALL))
-print("OK!")
+for i in range(0, len(lines), 2):
+    # 提取奇数行作为"function name"
+    function_name = lines[i].strip()
+    # 提取偶数行作为"function description"
+    function_description = lines[i + 1].strip()
+
+    # 创建JSON结构
+    json_data = {
+        "function name": function_name,
+        "function description": function_description
+    }
+
+    # 将JSON结构添加到列表中
+    json_list.append(json_data)
+
+# 将列表写入JSON文件
+with open("output.json", "w") as file:
+    json.dump(json_list, file, indent=4)
